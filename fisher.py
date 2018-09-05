08cb4418cbc2ada32d2d403425f878a08c375d90
suboqi <422819555@qq.com>
2018-09-05 10:28:47 +0800

    v3.4 搜索的关键字，判断isbn还是key

--- fisher.py
+++ fisher.py
@@ -2,9 +2,22 @@
 # -*- coding:utf-8 -*-
 
 from flask import Flask, make_response
+from helper import is_isbn_or_key
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+
+@app.route('/book/search/<q>/<page>')
+def search(q, page):
+    """
+    q: 关键字 或者 isbn
+    page:
+    # isbn isbn13 13个0到9的数字组成
+    # isbn10 10个0到9数字组成，含有一些 '-'
+    """
+    isbn_or_key = is_isbn_or_key(q)
+    pass
 
 
 @app.route('/hello')
