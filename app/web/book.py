24f976d9c66c7d66391b8a2f3997a1859d893419
suboqi <422819555@qq.com>
2018-09-06 12:02:28 +0800

    v7.7 使用__dict__序列化对象；从json序列化看代码解释权反转；

--- app/web/book.py
+++ app/web/book.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from flask import jsonify, request
+import json
 
 from app.forms.book import SearchForm
 from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
-from app.view_models.book import BookViewModel, BookCollection
+from app.view_models.book import BookCollection
 
 from . import web
-
-
-@web.route('/test')
-def test1():
-    from flask import request
-    from app.libs.none_local import n
-    print(n.v)
-    n.v = 2
-    print('-' * 35)
-    print(getattr(request, 'v', None))
-    setattr(request, 'v', 2)
-    print('-' * 35)
-    return ''
 
 
 @web.route('/book/search')
@@ -36,17 +24,22 @@ def search():
     # args = request.args.to_dict()
     form = SearchForm(request.args)
     books = BookCollection()
+
     if form.validate():
         q = form.q.data.strip()
         page = form.page.data
 
         isbn_or_key = is_isbn_or_key(q)
         yushu_book = YuShuBook()
+
         if isbn_or_key == 'isbn':
             yushu_book.search_by_isbn(q)
         else:
             yushu_book.search_by_keyword(q, page)
+
         books.fill(yushu_book, q)
-        return jsonify(books)
+        r = json.dumps(books, default=lambda o: o.__dict__)
+        return r
+        # return jsonify(books)
     else:
         return jsonify(form.errors)
